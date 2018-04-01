31100b1feacda175117762039fd4ecf0eb68c01b
kwh <758306135@qq.com>
2018-04-01 21:25:17 +0800

    我是主分支提交的数据

--- app/src/main/java/com/example/halou/myapplication/MainActivity.java
+++ app/src/main/java/com/example/halou/myapplication/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //我是我是主分支上进行开发的操作
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
