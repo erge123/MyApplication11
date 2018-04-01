799a7e312b92989cfa3db2693540a48f347dd534
kwh <758306135@qq.com>
2018-04-02 00:42:03 +0800

    Merge remote-tracking branch 'origin/wuWorking' into wuWorking
    # Conflicts:
    #	app/src/main/java/com/example/halou/myapplication/MainActivity.java

--- app/src/main/java/com/example/halou/myapplication/MainActivity.java
+++ app/src/main/java/com/example/halou/myapplication/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //123
+    //我是
+    //你还好吗
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
