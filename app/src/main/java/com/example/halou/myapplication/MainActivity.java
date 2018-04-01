b323b1ec4bfbfc36f975db37ea907c7610f50ac8
kwh <758306135@qq.com>
2018-04-01 22:34:39 +0800

    我是吴的分支上进行提交的数据

--- app/src/main/java/com/example/halou/myapplication/MainActivity.java
+++ app/src/main/java/com/example/halou/myapplication/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //我是在吴的分支上进行的切换的操作
+    //我是测试分支上的数据
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
