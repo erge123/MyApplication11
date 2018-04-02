75304dd1b32d61bcb25800b9a5842a5f66e50042
kwh <758306135@qq.com>
2018-04-02 09:51:25 +0800

    //无心帮删除的代码

--- app/src/main/java/com/example/halou/myapplication/MainActivity.java
+++ app/src/main/java/com/example/halou/myapplication/MainActivity.java
@@ -4,18 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //我不知道好不好
-    //1
-    //我是测试分支上的数据
-    //123
-    //我是康闻寒分支
-    /**
-     * 456
-     * @param savedInstanceState
-     */
-    //123
-    //为什么合并不了代码
-    //吴新棒再一次提交代码
+
+
+    //我是无心帮的操作的区域
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
